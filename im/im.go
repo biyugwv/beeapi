b9a2e0a31e7b6a0788434b87798ae61df4740361
biyugwv <biyugwv@163.com>
2019-12-23 17:03:34 +0800

    update im/im.go

--- im/im.go
+++ im/im.go
@@ -15,7 +15,7 @@ type Client struct {
     Conn *websocket.Conn    // 用户websocket连接
     SSID string             // 登陆后的验证Token
     PingTime int64          // 心跳监测 客户端发起  5秒收不到自动断开
-    PongTime int64          // 心跳监测 服务端发起每隔1秒左右发送一次
+    PongTime int64          // 收到客户端ping消息后发送到客户端
     Userid string           // 登录用户名
     ConnectTime int64
     lastMsgTime int64
@@ -25,14 +25,15 @@ type Client struct {
 type Message struct {
     EventType int8  `json:"type"`       //  -2 表示pong； -1 表ping ； 0表示用户发布消息；1表示用户进入；2表示用户退出；
     Name string     `json:"name"`       // 用户名称
-    Message string  `json:"message"`    // 消息内容
-}
-type msgGet struct {                 // 接收到的消息
-    msgtype  int8 `json:"msgtype"` 
-    msg   string `json:"msg"` 
-    SSID  string `json:"SSID"` 
+    Message string  `json:"msg"`    // 消息内容
 }
 
+// 接收到的消息
+type receiveMsg  struct {
+    MsgType int8  `json:"type"`       
+    Msg string     `json:"msg"`       
+    SSID string  `json:"SSID"`    
+}
 
 var (
     Clients map[string] Client
@@ -41,16 +42,7 @@ var (
     Msg   chan Message
 )
 
-func readMsgLoop(){
-    for {
-        for _,client := range Clients {
-            // 监听connection发送消息
-            readMsg(client)
-           
-        }
-        time.Sleep( 1 * time.Second )
-    }
-}
+
 
 func channelLoop(){  // 三个channel为空 读取操作会造成阻塞
     for{
@@ -64,22 +56,24 @@ func channelLoop(){  // 三个channel为空 读取操作会造成阻塞
                         fmt.Println("json.Marshal 错误")
                         return
                     }
-                    // 转换成字符串类型便于查看
-                    if err := client.Conn.WriteMessage(websocket.TextMessage, data) ; err != nil {
+                    
+                    if err := client.Conn.WriteMessage(websocket.TextMessage, data) ; err != nil {  // 转换成字符串类型便于查看
                         fmt.Println(err)
                     }
                     fmt.Println(string(data))
+                    fmt.Printf("当前在线人数：%d",len(Clients))
                 }
             case client := <- Join:
                 Clients[client.SSID] = client
-                msg := Message{1,"system",fmt.Sprintf("%s加入了房间",client.SSID)}
+                go readMsg(client)  // 监听connection发送消息
+                msg := Message{1,"system",fmt.Sprintf("%s进入了房间",client.SSID)}
                 Msg<-msg
                 
             case client := <- Leave:
                 msg := Message{2,"system",fmt.Sprintf("%s离开了房间",client.SSID)}
                 Msg<-msg
                 delete(Clients, client.SSID)
-                client.Conn.Close()
+               
         }
         
     }
@@ -88,43 +82,43 @@ func channelLoop(){  // 三个channel为空 读取操作会造成阻塞
 
 //  
 func readMsg(client Client){
-    now  := time.Now().Unix()
-    // 心跳判断
-    if client.PingTime == 0 {
-        client.PingTime = now
-    } 
-    if now - client.PingTime>= pingMaxTime{  // 超时没有收到客户端的心跳监测消息 将处理掉该链接
-        Leave <- client
-        return 
-    }
-    
-    var (
-        data []byte
-        err error
-    )
-    if _,data,err = client.Conn.ReadMessage() ; err!=nil {
-        Leave <- client
-        return 
-    }
-    msgget := &msgGet{}
-    json.Unmarshal(data,msgget)
-    fmt.Println(msgget)
-    fmt.Println(string(data))
-    // 心跳数据接收
-    if msgget.msgtype == -1 {
-        client.PingTime = now
-        // 向客户端发送监测数据
-        pong(client)
-    }else if  msgget.msgtype == 0 {
-        msg := Message{0,client.SSID,msgget.msg}
-        Msg<-msg
-    }else {
+    for{
+        if _,ok :=  Clients[client.SSID] ; !ok {  
+            fmt.Println("one client remove")
+            client.Conn.Close()
+            break
+        }
+        now  := time.Now().Unix()
+        // 超时没有收到客户端的心跳监测消息 将处理掉该链接
+        if client.PingTime == 0 {
+            client.PingTime = now
+        } 
+        if now - client.PingTime>= pingMaxTime{  
+            Leave <- client
+            return 
+        }
+        // 接收数据
+        var (
+            data []byte
+            err error
+        )
+        if _,data,err = client.Conn.ReadMessage() ; err!=nil {
+            Leave <- client
+            return 
+        }
+        msgget := receiveMsg{}
+        json.Unmarshal(data,&msgget)
         
-        fmt.Println("invilid data：%s",string(data))
+        if msgget.MsgType == -1 { // 心跳数据接收
+            client.PingTime = now
+            pong(client)  // 向客户端发送监测数据
+        }else if  msgget.MsgType == 0 {      // 此处会报interface{} is  nil  , not int  处理办法是先进行nil判断
+            msg := Message{0,client.SSID,msgget.Msg}
+            Msg<-msg
+        }else {
+            fmt.Println("invilid data：%s",string(data))
+        }
     }
-
-    // 向客户端发送消息 
-    
 }
 
 
@@ -151,5 +145,6 @@ func Run(){
     Msg = make(chan Message ,1000)
 
     go channelLoop()
-    go readMsgLoop()
+
 }
+
